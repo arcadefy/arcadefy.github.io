b4f5ddc03551310d7bbd46c0f33a15200e3724fd
M Jayasurya <67519756+oksurya@users.noreply.github.com>
2025-04-09 21:43:47 +0530

    sdf

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-
+        <meta name="google-site-verification" content="IiBDXLQ0PxMoOZrgzj3qmqv1iSt_7bs4gSLQsno0X6c" />
       </Head>
       <body className="antialiased">
         <Main />
